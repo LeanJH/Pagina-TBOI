3bdc089fb3c731126b4ebebe708cb238ba3e033d
LeanJH <80850164+LeanJH@users.noreply.github.com>
2026-06-08 16:52:53 -0500

    Delete taller por un punto directory

--- taller por un punto/punto1.py
+++
@@ -1,8 +0,0 @@
-# Lista de stocks
-inventario = {"frutas": 10, "pan": 4, "leche": 3}
-
-for producto, cantidad in inventario.items():
-    if cantidad < 5:
-        print(f"{producto} está por acabarse, pedir más al proveedor.")
-    else:
-        print(f"{producto} tiene suficiente stock ({cantidad} unidades).")
--- taller por un punto/punto2.py
+++
@@ -1,6 +0,0 @@
-#clasificacion de edad para peliculas
-edad= int(input("ingrese la edad del usuario:"))
-if edad >= 18:
-    print("puede entrar a la clasificacion de Terror")
-else:
-    print("Debes de ver peliculas para menores de edad")
