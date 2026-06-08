3a9b15666182ef206a661e940a5f1cb9640f6506
LeanJH <80850164+LeanJH@users.noreply.github.com>
2026-06-08 16:53:01 -0500

    Delete taller directory

--- taller/punto10.py
+++
@@ -1,9 +0,0 @@
-#lista de compra
-productos = [{"nombre": "huevo", "precio": 200, "cantidad": 3},{"nombre": "1 kg azucar", "precio": 3500, "cantidad": 2},{"nombre": "pan", "precio": 2200, "cantidad": 2}]
-print("factura de compra")
-total = 0
-for producto in productos:
-    subtotal = producto["precio"] * producto["cantidad"]
-    print(f"{producto['nombre']} x {producto['cantidad']} = ${subtotal}")
-    total += subtotal
-print(f"total: ${total}")
--- taller/punto7.py
+++
@@ -1,8 +0,0 @@
-#dias lluviosos
-dias_lluviosos = 0
-mes = int(input("¿Cuántos días tiene el mes a registrar? "))
-for dia in range(1, mes + 1):
-    reporte = input(f"¿Llovió el día {dia}? (s/n): ").lower()
-    if reporte == "s":
-        dias_lluviosos += 1
-print(f"El total de días lluviosos en el mes fue: {dias_lluviosos}")
